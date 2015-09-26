833b480c301f8d653ca64532d871af9bd588ca55
ryan <ryanjbrackney@gmail.com>
2015-09-26 15:23:09 -0700

    website ready for loading on server

--- app/views.py
+++ app/views.py
@@ -14,18 +14,29 @@ import numpy as np
 with open('db.pw') as f: #ignored in git
     pw = f.read().strip('\n')
 
-#db = mdb.connect(user="root", host="localhost", passwd=pw, db="world", charset='utf8')
-db = mdb.connect(user="root", host="localhost", passwd=pw, db="gamepricepred", charset='utf8')
+
+#db = mdb.connect(user="root", host="localhost", passwd=pw, db="gamepricepred", charset='utf8') #removed for now
+db2 = mdb.connect(user="root", host="localhost", passwd=pw, db="ssf_db", charset='utf8')
+
+with db2:
+    cur = db2.cursor()
+    cur.execute("SELECT game_name,appid FROM website_table")
+
+OPTION_LIST = cur.fetchall()    
+#option_list = sorted([x[0] for x in option_list])
+OPTION_LIST = sorted([{'name':x[0],'appid':x[1]} for x in OPTION_LIST],key=lambda k: k['name'] )
+
 @app.route('/')
 @app.route('/index')
 def ss_input():
-    option_list=['a','b','c','d','e','f','g']
-    return render_template("ssinput.html",option_list = option_list) #currently the index  
+    #db2 = mdb.connect(user="root", host="localhost", passwd='hats', db="ssf_db", charset='utf8')
+    
+    return render_template("ssinput.html",option_list = OPTION_LIST) #currently the index  
 
 @app.route('/ssoutput')
 def ss_output():
-    game_id =  request.args.get('ID')
-    
+    #game_id =  request.args.get('ID')
+    game_id =  int(request.args.get('option'))
     with db:
         cur = db.cursor()
         #add try/except statements
@@ -35,18 +46,30 @@ def ss_output():
     game_info = str(query_results[0]) #just return the first option
 
     appid= game_id
-    x = np.array([1,2,3,4,5,6,7,8,9,10])
-    y = np.array([1,2,3,4,5,6,7,8,9,10])
-    graph_response = a_Model.graph_prices(x,y,'stuff')
-    return render_template("ssoutput.html", game_info = game_info, appid = appid)
+    
+    db2 = mdb.connect(user="root", host="localhost", passwd=pw, db="ssf_db", charset='utf8')
+    with db2:
+        cur = db2.cursor(mdb.cursors.DictCursor)
+        cur.execute("SELECT * FROM website_table WHERE appid = %d" % appid)
 
-@app.route("/priceplot.png")
-def priceplot():
-    '''modified from example at https://gist.github.com/wilsaj/862153'''
-    x = np.array([1,2,3,4,5,6,7,8,9,10])
-    y = np.array([1,2,3,4,5,6,7,8,9,10])
-    response = a_Model.graph_prices(x,y,'stuff')
-    return response
+    ginfo = cur.fetchone()
+    gname = ginfo['game_name']
+    adiscount = "%0.1f%%" % (ginfo['avg_discount'])
+    asavings = "$%0.2f" % (ginfo['avg_savings']) 
+    percent = "%0.1f%%" % (ginfo['sale_prob_y']*100)
+    
+    ## stand in for real code:
+    if ginfo['sale_prob_y'] < 0.5:
+        buy_time = 'buy it now, since it is not likely to go on sale soon.'
+    else:
+        buy_time = 'wait for a sale.'
+    
+    return render_template("ssoutput.html", game_info = game_info, 
+                           appid = appid,option_list = OPTION_LIST,
+                           name = gname, discount = adiscount, 
+                           savings = asavings, percent = percent,
+                           buy_time = buy_time)
+
 
 @app.route("/<appid>/priceplot.png")
 def priceplot2(appid):    
